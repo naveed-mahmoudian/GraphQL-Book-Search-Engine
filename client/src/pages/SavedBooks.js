14244bce4f5c52bc7f869f4ad9b9b75229f45187
Naveed Mahmoudian <nmahmoudian@gmail.com>
2023-01-24 15:59:16 -0800

    clean up

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,7 +7,6 @@ import {
   Button,
 } from "react-bootstrap";
 
-// import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 import { useMutation, useQuery } from "@apollo/client";
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -85,12 +85,6 @@ const SearchBooks = () => {
           link: bookToSave.link,
         },
       });
-      console.log(saveBookData);
-      // const response = await saveBook(bookToSave, token);
-
-      // if (!response.ok) {
-      //   throw new Error("something went wrong!");
-      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
